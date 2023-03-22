e56c9254ee14dc500aec839691b0b89b80456703
mariuszchucherko <mariuszchucherko@workhuman.com>
2023-03-22 08:14:14 +0000

    365. Assigment - Create Data Model.

---
+++ src/main/java/guru/springframework/api/domain/Billing.java
@@ -0,0 +1,46 @@
+
+package guru.springframework.api.domain;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "card"
+})
+@Generated("jsonschema2pojo")
+public class Billing {
+
+    @JsonProperty("card")
+    private Card card;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("card")
+    public Card getCard() {
+        return card;
+    }
+
+    @JsonProperty("card")
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
